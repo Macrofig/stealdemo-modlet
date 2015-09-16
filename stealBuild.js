e2082fdf5bd22d0f237ad75c62f01aad438fc50e
Juan Orozco <juanthedesigner@gmail.com>
2015-09-16 10:58:42 -0400

    Cleaned up modlet. Adding build script.

---
+++ stealBuild.js
@@ -0,0 +1,16 @@
+var stealTools = require("steal-tools");
+
+stealTools.export({
+	system: {
+		config: "package.json!npm"
+	},
+	options: {
+		sourceMaps: true
+	},
+	outputs: {
+		"+cjs": {},
+		"+amd": {},
+		"+global-js": {},
+		"+global-css": {}
+	}
+});
